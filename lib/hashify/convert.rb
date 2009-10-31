103f170b420c648918244742d75bef9797b35dc9
Joshua Hull <joshbuddy@gmail.com>
2009-10-31 14:57:29 -0400

    fixed scoping

--- lib/hashify/convert.rb
+++ lib/hashify/convert.rb
@@ -1,6 +1,6 @@
 module Hashify
   module Convert
     Pass = nil
-    Time = [Proc.new{|x| x.nil? ? nil : x.to_i}, Proc.new{|x| x.nil? ? nil : Time.at(x.to_i)}]
+    Time = [Proc.new{|x| x.nil? ? nil : x.to_i}, Proc.new{|x| x.nil? ? nil : ::Time.at(x.to_i)}]
   end
 end
